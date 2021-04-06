6b9c6c650d452c1ffe35f6f5ea0c6712329c9783
Trung Nguyễn <48853223+nttrung9x@users.noreply.github.com>
2021-04-06 10:47:21 +0700

    Update install_centos8x64_iponly.sh

--- scripts/install_centos8x64_iponly.sh
+++ scripts/install_centos8x64_iponly.sh
@@ -117,7 +117,7 @@ echo "Internal ip = ${IP4}. Exteranl sub for ip6 = ${IP6}"
 echo "2000 proxy to create !"
 COUNT=2000
 echo "input ip allow connect? Example 27.74.82.97 or Multi ip 27.74.82.97,27.74.82.98,27.74.82.99"
-read IPALLOWCONNECT
+#read IPALLOWCONNECT
 IPALLOWCONNECT="115.79.187.199,127.0.0.1"
 
 FIRST_PORT=10000
