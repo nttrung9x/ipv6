62b3f717820236ea6efe713262b7199d1d9b3255
Trung Nguyễn <48853223+nttrung9x@users.noreply.github.com>
2024-01-25 15:22:33 +0700

    Update test-v6-r48.sh

--- test-v6-r48.sh
+++ test-v6-r48.sh
@@ -28,7 +28,7 @@ install_3proxy() {
 #    systemctl enable 3proxy
     #echo "* hard nofile 999999" >>  /etc/security/limits.conf
     #echo "* soft nofile 999999" >>  /etc/security/limits.conf
-    #echo "net.ipv6.conf.ens3.proxy_ndp=1" >> /etc/sysctl.conf
+    #echo "net.ipv6.conf.em2.proxy_ndp=1" >> /etc/sysctl.conf
     #echo "net.ipv6.conf.all.proxy_ndp=1" >> /etc/sysctl.conf
     #echo "net.ipv6.conf.default.forwarding=1" >> /etc/sysctl.conf
     #echo "net.ipv6.conf.all.forwarding=1" >> /etc/sysctl.conf
@@ -94,7 +94,7 @@ EOF
 
 gen_ifconfig() {
     cat <<EOF
-$(awk -F "/" '{print "ifconfig ens3 inet6 add " $5 "/64"}' ${WORKDATA})
+$(awk -F "/" '{print "ifconfig em2 inet6 add " $5 "/64"}' ${WORKDATA})
 EOF
 }
 echo "installing apps"
@@ -113,8 +113,8 @@ IP4=$(curl -4 -s icanhazip.com)
 IP6="2a0c:7886:123"
 
 echo "Internal ip = ${IP4}. Exteranl sub for ip6 = ${IP6}"
-echo "1000 proxy to create !"
-COUNT=1000
+echo "2000 proxy to create !"
+COUNT=2000
 IPALLOWCONNECT="216.244.78.250,216.244.79.106,216.244.85.18,14.230.255.95"
 
 
@@ -123,16 +123,15 @@ LAST_PORT=$(($FIRST_PORT + $COUNT))
 
 gen_data >$WORKDIR/data.txt
 gen_iptables >$WORKDIR/boot_iptables.sh
-gen_ifconfig >$WORKDIR/boot_ifconfig.sh
+#gen_ifconfig >$WORKDIR/boot_ifconfig.sh
 chmod +x $WORKDIR/boot_*.sh /etc/rc.local
 
 gen_3proxy >/usr/local/etc/3proxy/3proxy.cfg
 
 cat >>/etc/rc.local <<EOF
 systemctl start NetworkManager.service
-ifup ens3
+ifup em2
 bash ${WORKDIR}/boot_iptables.sh
-bash ${WORKDIR}/boot_ifconfig.sh
 ulimit -n 65535
 /usr/local/etc/3proxy/bin/3proxy /usr/local/etc/3proxy/3proxy.cfg &
 EOF
